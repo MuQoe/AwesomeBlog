181a02f684b20b3603d059bf13f3bfdc427ca975
MuQoe <1478450488@qq.com>
2021-11-22 19:53:09 +0800

    [新增] UserModel

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module AwesomeBlog
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.2
 	github.com/kardianos/service v1.2.0
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/viper v1.9.0
 	go.uber.org/zap v1.19.1
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gorm.io/driver/mysql v1.2.0
 	gorm.io/gorm v1.22.3
--- internal/app/model/User.go
+++ internal/app/model/User.go
@@ -8,13 +8,13 @@ import (
 
 type User struct {
 	ID            uint           `json:"user_id" gorm:"primarykey"`                        // 用户ID
-	Identity      int            `json:"user_Identity"`                                    // 用户权限组
+	Identity      int8           `json:"user_Identity"`                                    // 用户权限组
 	UserName      string         `json:"username" gorm:"type:varchar(100);unique"`         // 用户名
 	Password      string         `json:"user_password"`                                    // 用户密码
 	Avatar        string         `json:"user_avatar"`                                      // 用户头像地址
 	Email         string         `json:"user_email" gorm:"type:varchar(100);unique_index"` // 用户Email 唯一值
 	Sign          string         `json:"user_sign"`                                        // 用户个性签名
-	Status        bool           `json:"user_status"`                                      // 用户状态(0未激活 1已激活 2已封禁)
+	Status        int8           `json:"user_status"`                                      // 用户状态(0未激活 1已激活 2已封禁)
 	LastLoginTime time.Time      `json:"user_lastlogintime"`                               // 上次登陆时间
 	CreatedAt     time.Time      `json:"user_createat"`                                    // 用户创建时间
 	DeletedAt     gorm.DeletedAt `json:"user_deleteat" gorm:"index"`                       // 用户删除时间
